cca5c87d0e0dd8045fabd7d76dac33c1ad970134
Shane Kobylecky <skobylecky1@gmail.com>
2023-08-12 23:32:14 -0700

    Add a navigation component with a single link

---
+++ src/components/Navigation.js
@@ -0,0 +1,13 @@
+import { NavLink } from "react-router-dom";
+
+const Navigation = () => {
+    return (
+        <nav>
+            <NavLink
+                to="/home" 
+            >Home</NavLink>
+        </nav>
+    )
+}
+
+export default Navigation;
