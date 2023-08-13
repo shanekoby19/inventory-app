6033c65d4b908853be7478724ccd4856cac59a9f
Shane Kobylecky <skobylecky1@gmail.com>
2023-08-12 23:30:48 -0700

    Add a front-end router using react-router-dom

---
+++ src/routes/router.js
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "../pages/Layout";
+import Home from "../pages/Home";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Layout />,
+        children: [
+            {
+                path: '/home',
+                element: <Home />
+            }
+        ]
+    }
+]);
+
+export default router;
