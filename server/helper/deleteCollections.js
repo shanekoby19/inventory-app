da9ea3ad479740240ed0e7443f91baba1ef7700c
Shane Kobylecky <skobylecky1@gmail.com>
2023-08-15 00:30:37 -0700

    Add a helper file to delete all collections in the database.

---
+++ server/helper/deleteCollections.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+require('dotenv').config({
+    path: 'server/config/dbconfig.env'
+});
+
+async function deleteAllCollections() {
+    const collections = await mongoose.connection.db.collections();
+  
+    for (let collection of collections) {
+      await collection.deleteMany();
+    }
+  
+    console.log('All collections removed.');
+    mongoose.connection.close();
+}
+
+const { DB_USERNAME, DB_PASSWORD } = process.env;
+const connectionString = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@cluster0.ta4kr3w.mongodb.net/?retryWrites=true&w=majority`;
+
+// REPLACE WITH REAL SERVER CONNECTION
+mongoose.connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => {
+  deleteAllCollections().catch(console.error)
+});
