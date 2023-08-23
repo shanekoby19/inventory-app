ad2fbf569876c3213665431ce04f6065392a1723
Shane Kobylecky <skobylecky1@gmail.com>
2023-08-22 23:20:57 -0700

    Update the user route and user controller to have custom logic for managing users

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,6 +3,81 @@ const Warehouse = require('../models/warehouse');
 
 const catchAsync = require('../utils/catchAsync');
 
+/**
+ * A function that gets an item from a collection the database given the item id.
+ * @param {*} Model - The model you want to search in.
+ * @param {string} idParam  - The id parameter name set in the url of the express route. Defaults to "id".
+ * @returns - A middleware function to use in express.
+ */
+const getUser = (Model, idParam="id") => {
+    return catchAsync(async(req, res, next) => {
+        // Extract the id.
+        const id = req.params[idParam];
+        
+        // Find the item given the id.
+        const user = await Model.findById(id);
+    
+        // Return the item you found.
+        res.status(200).json({
+            [Model.modelName.toLowerCase()]: user
+        })
+    })
+}
+
+/**
+ * A function that gets all items from a collection in the database.
+ * @param {*} Model - The model you want to search in.
+ * @returns - A middleware function to use in express.
+ */
+const getAllUsers = (Model) => {
+    return catchAsync(async(req, res, next) => {
+        // Find all instances in the model.
+        const users = await Model.find();
+
+        res.status(200).json({
+            [`${Model.modelName.toLowerCase()}s`]: users
+        })
+    })
+}
+
+/**
+ * A utility function to create objects in the database given a body and some data.
+ * @param {*} Model - The model you want to use to add this object to the database.
+ * @returns - A middleware function to use in express.
+ */
+const createUser = (Model) => {
+    return catchAsync(async(req, res, next) => {
+        // req.data will contain any valid data passed in that matches the model.
+        const user = await Model.create(req.data);
+
+        res.status(201).json({
+            [Model.modelName.toLowerCase()]: user
+        })
+    })
+}
+
+/**
+ * A utility function to update objects in the database given a body and some data.
+ * @param {*} Model - The model you want to use to update this object in the database.
+ * @param {string} idParam  - The id parameter name set in the url of the express route. Defaults to "id"
+ * @returns - A middleware function to use in express.
+ */
+const updateUser = (Model, paramId="id") => {
+    return catchAsync(async(req, res, next) => {
+        // Extract the id from the incoming request.
+        const id = req.params[paramId];
+
+        const user = await Model.findByIdAndUpdate(id, req.data, {
+            new: true,
+            runValidators: true,
+        });
+
+        res.status(200).json({
+            [Model.modelName.toLowerCase()]: user
+        })
+    })
+}
+
 const deleteUser = catchAsync(async(req, res, next) => {
     const userId = req.params.id;
 
@@ -18,5 +93,9 @@ const deleteUser = catchAsync(async(req, res, next) => {
 });
 
 module.exports = {
+    getUser,
+    getAllUsers,
+    createUser,
+    updateUser,
     deleteUser
 }
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,10 +2,6 @@ const express = require('express');
 const User = require('../models/user');
 
 const {
-    get,
-    getAll,
-    create,
-    update,
     addDataPropToRequestBody
 } = require('../utils/utilsController');
 
@@ -14,20 +10,60 @@ const {
 } = require('../controllers/authController');
 
 const {
-    deleteUser
+    deleteUser, 
+    getAllUsers, 
+    createUser, 
+    getUser, 
+    updateUser
 } = require('../controllers/userController');
+const AppError = require('../utils/AppError');
 
 const userRouter = express.Router();
 
+const isAuthorizedToCreateUser = (onlyAllowedRole) => {
+    return (req, res, next) => {
+        // If the logged in user is not an admin and tries to create an admin, throw an error.
+        if(onlyAllowedRole === 'user' && req.body.role === 'admin') {
+            return next(new AppError('You do not have the credentials to create this resource.'))
+        } else if(onlyAllowedRole === 'admin' && req.user.role !== 'admin') {
+            return next(new AppError('You do not have the credentials to create this resource.'))
+        }
+        next();
+    }
+}
+
+const isAuthorizedToViewOrUpdateUser = () => {
+    return (req, res, next) => {
+        // If the logged in user is not an admin and tries to view a user other than themselve, throw an error.
+        if(req.user.role === 'user' && req.user._id !== req.params.id) {
+            return next(new AppError('You do not have the credentials to view or update this resource.'))
+        }
+        next();
+    }
+}
+
+const isAdmin = () => {
+    return (req, res, next) => {
+        if(req.user.role !== 'admin') {
+            return next(new AppError('You do not have the credentials to perform this action.'))
+        }
+        next();
+    }
+}
+
 userRouter
     .route('/')
-    .get(isAuthenticated, getAll(User))
-    .post(addDataPropToRequestBody(User), create(User));
+    .get(isAuthenticated, isAdmin(), getAllUsers(User))
+    .post(addDataPropToRequestBody(User), isAuthorizedToCreateUser('user'), createUser(User));
 
 userRouter
     .route('/:id')
-    .get(isAuthenticated, get(User))
-    .delete(isAuthenticated, deleteUser)
-    .patch(isAuthenticated, addDataPropToRequestBody(User), update(User));
+    .get(isAuthenticated, isAuthorizedToViewOrUpdateUser(), getUser(User))
+    .delete(isAuthenticated, isAdmin(), deleteUser)
+    .patch(isAuthenticated, isAuthorizedToViewOrUpdateUser(), addDataPropToRequestBody(User), updateUser(User));
+
+userRouter
+    .route('/admin')
+    .post(isAuthenticated, addDataPropToRequestBody(User), isAuthorizedToCreateUser('admin'), createUser(User))
 
 module.exports = userRouter;
